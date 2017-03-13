8b5867a5f98ca2452fc3e9075b12230fe0f88981
Casey Morris <casemorrisj@gmail.com>
2017-03-13 08:41:26 -0700

    Added Handlebars to Spark app

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,5 +1,8 @@
 package com.teamtreehouse.courses;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.*;
 
 /**
@@ -7,8 +10,8 @@ import static spark.Spark.*;
  */
 public class Main {
     public static void main(String[] args){
-
-        get("/hello", (req, res) -> "Hello World");
-        get("/", (req, res)-> "Hello");
+        get("/", (req, res)-> {
+           return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
